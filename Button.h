f1dc6088faf7b9c7b2ff1056b58cb0e7085e0def
nvs174 <nejur02@mail.ru>
2023-12-01 10:16:33 +0300

    Create Button.h

---
+++ Button.h
@@ -0,0 +1,29 @@
+#ifndef BUTTON
+#define BUTTON
+#include "IButton.h"
+#include "Gerland.h"
+#include <array>
+
+template<typename Port, int pinNumber>
+class Button : public IButton
+{
+public:
+  bool IsPressed() override   
+  {
+     if(((Port::IDR::Get()&(1 << pinNumber))==0) && (isSet))
+  {
+    isSet = false;
+  } else if(((Port::IDR::Get()&(1 << pinNumber))==(1 << pinNumber)) && (!isSet))
+  {
+    isSet = true;
+    return true;
+  }
+  return 0;
+    
+  } 
+private:
+  bool isSet = true;
+};
+
+
+#endif
